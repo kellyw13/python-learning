5dc98cad7734a44709c5d8e4609ddd10498a8770
Kellyw13 <kellywilliamson1000@gmail.com>
2024-10-05 19:34:37 +0200

    added function to fit distribution and print parameters

--- statistics-assignment/distribution.py
+++ statistics-assignment/distribution.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.stats import norm, uniform, expon
 
-#import dataset from excel
+# Import dataset from excel
 data = pd.read_excel("grupp7.xlsx", sheet_name="Uppgift3")
+
+
+# Function to fit distribution for each dataset
+def fit_and_print_dist_params(data, column_name):
+    # fit exponential distribution
+    loc, scale = expon.fit(data)
+    print(f'Exponential distribution for {column_name}: rate (lambda) = {1/scale}, scale = {scale}')
+    
+    # fit Normal distribution
+    mu, std = norm.fit(data)
+    print(f'Normal distribution for {column_name}: mean = {mu}, std = {std}')
+        
+    # fit Uniform distribution
+    a, b = uniform.fit(data)
+    print(f'Uniform distribution for {column_name}: lower-bound = {a}, upper-bound = {b}')
 
 #Plot the histogram for each dataset in the excel file
 for column in data.columns:
